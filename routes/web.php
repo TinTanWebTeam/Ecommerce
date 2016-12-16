1f70d4e85f1b7398e589b09b1e91d735ed094fa5
nguyentrucxinh <nguyentrucxjnh@gmail.com>
2016-12-16 09:22:39 +0700

    complete view

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,59 @@ Route::get('/checkout', function () {
 Route::get('/wishlist', function () {
     return view('subviews.wishlist');
 });
+
+Route::get('/about', function () {
+    return view('subviews.about');
+});
+
+Route::get('/blog-detail', function () {
+    return view('subviews.blog-detail');
+});
+
+Route::get('/blog-page', function () {
+    return view('subviews.blog-page');
+});
+
+Route::get('/compare', function () {
+    return view('subviews.compare');
+});
+
+Route::get('/contact', function () {
+    return view('subviews.contact');
+});
+
+Route::get('/faq', function () {
+    return view('subviews.faq');
+});
+
+Route::get('/gift-voucher', function () {
+    return view('subviews.gift-voucher');
+});
+
+Route::get('/login', function () {
+    return view('subviews.login');
+});
+
+Route::get('/register', function () {
+    return view('subviews.register');
+});
+
+Route::get('/my-account', function () {
+    return view('subviews.my-account');
+});
+
+Route::get('/order-history', function () {
+    return view('subviews.order-history');
+});
+
+Route::get('/order-information', function () {
+    return view('subviews.order-information');
+});
+
+Route::get('/return', function () {
+    return view('subviews.return');
+});
+
+Route::get('/sitemap', function () {
+    return view('subviews.sitemap');
+});
